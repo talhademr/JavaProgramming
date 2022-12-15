cadeb1c737a0540e7b48fd562179a8b26e20ebcb
Talha Demir <talha.demir06@gmail.com>
2022-12-15 09:38:43 +0300

    creating Square class

--- JavaProgramming/src/day39_Recap/shapeTask/Square.java
+++ JavaProgramming/src/day39_Recap/shapeTask/Square.java
@@ -1,5 +1,26 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side <=0){
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public Square( double side) {
+        super("Square");
+        setSide(side);
+    }
+
+
 
 }
