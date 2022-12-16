8b705d5b1f03f1ec0cf2fe6f1512b2f1b8c42d98
Talha Demir <talha.demir06@gmail.com>
2022-12-16 08:15:02 +0300

    creating ShapeObjectsobject class

---
+++ JavaProgramming/src/day39_Recap/shapeTask/ShapeObjects.java
@@ -0,0 +1,34 @@
+package day39_Recap.shapeTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Square square = new Square(10);
+
+        System.out.println(square);
+
+        square.setSide(15);
+
+        System.out.println(square);
+
+
+        System.out.println("------------------------");
+
+
+        Rectangle rectangle = new Rectangle(12.0,6);
+
+        System.out.println(rectangle);
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
