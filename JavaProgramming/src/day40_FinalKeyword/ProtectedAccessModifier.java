ac022a2f884a541a8685a33b35c5d5374abed391
Talha Demir <talha.demir06@gmail.com>
2022-12-22 09:10:36 +0300

    day 40 created

---
+++ JavaProgramming/src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,12 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo"; //default access modifier
+    protected static String name2 = "WoodenSpoon"; // protected
+
+    //bunları day40 dışındaki packagelarda ulaşmayı deneyerek test edebilirsin.
+
+    //protected olanı subclass dışında erişemeyiz. default a ise hiç erişemeyiz
+
+}
