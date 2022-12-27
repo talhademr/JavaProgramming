31d6aaccf3299fa2c652c97dc9a0f41100747fe9
Talha Demir <talha.demir06@gmail.com>
2022-12-27 09:16:16 +0300

    day 42 created

---
+++ JavaProgramming/src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,23 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("----------Test 1------") ;
+
+        System.out.println("hello");
+        Thread.sleep(3000);
+        System.out.println("cydeo");
+
+        System.out.println("test 1 completed");
+
+        System.out.println("hello");
+        Thread.sleep(3000);
+        System.out.println("batch eu10");
+
+        System.out.println("--------------test 2 completed-----------");
+
+
+
+    }
+}
