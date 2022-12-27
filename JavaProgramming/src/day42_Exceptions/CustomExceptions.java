daa7fd1c8dbb0f7121e91791d2b8af7e0f9bb28f
Talha Demir <talha.demir06@gmail.com>
2022-12-27 09:58:30 +0300

    day 42 created

--- JavaProgramming/src/day42_Exceptions/CustomExceptions.java
+++ JavaProgramming/src/day42_Exceptions/CustomExceptions.java
@@ -16,11 +16,19 @@ class FadyException extends RuntimeException {
     }
 
     public class CustomExceptions {
-        public static void main(String[] args) {
+        public void main(String[] args) {
 
-            throw new FadyException("It's time for breake");
+            // throw new FadyException("It's time for lunch break");
+
+
+            try {
+                throw new NoBreakException();
+            } catch (NoBreakException e) {
+                e.printStackTrace();
+            }
 
 
         }
+
     }
 }
